8f08599f32492c0a910016b30fec3e7a73186974
rqsell <68514875+rqsell@users.noreply.github.com>
2021-04-16 15:22:34 -0400

    image overlay complete

--- src/App.js
+++ src/App.js
@@ -16,7 +16,16 @@ function App() {
      <body>
        <div className="container">
          <img src="./airplane.jpg" className="airplane"/>
-         <div class="overlay overlayLeft"></div>
+         
+         <div class="overlay overlayLeft">
+         <div className="textOverlay">
+           <h1>Aircraft Maintenance Tracking Digital</h1>
+           <div className= "about">
+             <h2> About Us</h2>
+<span>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin a sem felis. Fusce placerat nisi dictum elit condimentum, id ornare velit lobortis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Morbi at dolor feugiat nunc luctus semper non ac ligula. Duis eu urna pharetra nisi lacinia venenatis. Aenean maximus consectetur arcu, eu varius leo hendrerit ut. Quisque varius orci id sapien tincidunt, quis maximus tellus posuere.</span>
+           </div>
+         </div>
+         </div>
        </div>
      </body>
     </div>
