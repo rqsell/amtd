d618e13996aa781e6f1c8fd3061a9ef1e2080320
rqsell <68514875+rqsell@users.noreply.github.com>
2021-04-16 14:09:41 -0400

    craeting nav

--- src/App.js
+++ src/App.js
@@ -4,20 +4,14 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <nav className="navbar">
+       <ul>
+       <li><img src="./logo.png"/></li>
+         <li>Features</li>
+         <li>Services</li>
+         <li>Support</li>
+       </ul>
+     </nav>
     </div>
   );
 }
