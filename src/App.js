128671c980cbc7b76e5d970bfb4e51f0840bc4ad
rqsell <68514875+rqsell@users.noreply.github.com>
2021-04-16 15:31:41 -0400

    footer added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from "./Home.js"
 
 function App() {
   return (
@@ -13,21 +14,12 @@ function App() {
          <li>Login</li>
        </ul>
      </nav>
-     <body>
-       <div className="container">
-         <img src="./airplane.jpg" className="airplane"/>
-         
-         <div class="overlay overlayLeft">
-         <div className="textOverlay">
-           <h1>Aircraft Maintenance Tracking Digital</h1>
-           <div className= "about">
-             <h2> About Us</h2>
-<span>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin a sem felis. Fusce placerat nisi dictum elit condimentum, id ornare velit lobortis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Morbi at dolor feugiat nunc luctus semper non ac ligula. Duis eu urna pharetra nisi lacinia venenatis. Aenean maximus consectetur arcu, eu varius leo hendrerit ut. Quisque varius orci id sapien tincidunt, quis maximus tellus posuere.</span>
-           </div>
-         </div>
-         </div>
-       </div>
-     </body>
+     <Home/>
+     <footer className="footer">
+       <span>
+         Last Updated 04/16/2021
+       </span>
+     </footer>
     </div>
   );
 }
