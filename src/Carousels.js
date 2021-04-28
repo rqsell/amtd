5feb680741526d0a71af1b6a9c0f4bed32919c3f
rqsell <68514875+rqsell@users.noreply.github.com>
2021-04-28 12:48:18 -0400

    front page edits

--- src/Carousels.js
+++ src/Carousels.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Carousel from 'react-bootstrap/Carousel'
+import "./styles/Carousels.css"
 
 function Carousels(props) {
     return (
-        <div>
-           <Carousel>
-  <Carousel.Item interval={1000}>
+        <div className= "caropage">
+           <Carousel classname="caro">
+  <Carousel.Item interval={1000}  className="caroitem">
     <img
       className="d-block w-100"
       src="./jetimg2.jpg"
@@ -16,22 +17,25 @@ function Carousels(props) {
       <p>Aircraft Maintenance Tracking Digital is a simplified and easy tool that lets you instantly check the status of upkeep for all of your aircrafts. Manage tasks, check dates and verify all the information you need to know to ensure safety.  </p>
     </Carousel.Caption>
   </Carousel.Item>
-  <Carousel.Item interval={500}>
+  <Carousel.Item interval={500}  className="caroitem">
     <img
       className="d-block w-100"
+      id="caroimg"
       src="./jetimg3.jpg"
       alt="Second slide"
+    
     />
     <Carousel.Caption>
       <h3>Simplified Dashboard</h3>
       <p>We prioritize tracking your maintenance needs using our simplified and easy to use dashboard.</p>
     </Carousel.Caption>
   </Carousel.Item>
-  <Carousel.Item>
+  <Carousel.Item className="caroitem">
     <img
       className="d-block w-100"
       src="./jetimg4.jpg"
       alt="Third slide"
+      
     />
     <Carousel.Caption>
       <h3>Built For You</h3>
@@ -39,6 +43,20 @@ function Carousels(props) {
     </Carousel.Caption>
   </Carousel.Item>
 </Carousel> 
+<div className="about">
+<h1>About Us</h1>
+<p>Aircraft Maintenance Tracking Digital is a simplified and easy tool that lets you instantly check the status of upkeep for all of your aircrafts. Manage tasks, check dates and verify all the information you need to know to ensure safety.  </p>
+</div>
+<div className="details">
+<section>
+<h3>Simplified Dashboard</h3>
+      <p>We prioritize tracking your maintenance needs using our simplified and easy to use dashboard.</p>
+</section>
+<section>
+<h3>Built For You</h3>
+      <p>Aircraft Maintenance Tracking Digital was built to streamline the tracking process with an easy to use interface. </p>
+</section>
+        </div>
         </div>
     );
 }
--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import "./Home.css"
-
+import "./styles/Home.css"
+import Carousels from "./Carousels"
 function Home(props) {
     return (
         <div>
@@ -9,9 +9,9 @@ function Home(props) {
        <h1 className= "header">Aircraft Maintenance Tracking Digital</h1>
        
            
-         
+       <Carousels {...props}/>    
        <div className="container">
-         <img src="./jetimg2.jpg" className="airplane"/>
+
          
          {/* Textbox for Mobile */}
          <div className= "mobiletextbox">
@@ -25,6 +25,9 @@ function Home(props) {
            <div className= "about">
              <h2> About Us</h2>
   <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin a sem felis. Fusce placerat nisi dictum elit condimentum, id ornare velit lobortis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Morbi at dolor feugiat nunc luctus semper non ac ligula. Duis eu urna pharetra nisi lacinia venenatis. Aenean maximus consectetur arcu, eu varius leo hendrerit ut. Quisque varius orci id sapien tincidunt, quis maximus tellus posuere.</span>
+           </div>
+           <div className="details">
+
            </div>
          </div>
          </div>
